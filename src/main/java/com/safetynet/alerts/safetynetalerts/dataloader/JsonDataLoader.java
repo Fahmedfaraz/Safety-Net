19e08e19a2c4d15679ff568face7cc675feb61fc
afk..007fs <shamim.faiza@gmail.com>
2025-03-23 15:45:21 -0400

    loading JSON data after cleanup

--- src/main/java/com/safetynet/alerts/safetynetalerts/dataloader/JsonDataLoader.java
+++ src/main/java/com/safetynet/alerts/safetynetalerts/dataloader/JsonDataLoader.java
@@ -6,13 +6,10 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.safetynet.alerts.safetynetalerts.rest.beans.FireStation;
@@ -24,8 +21,6 @@ import com.safetynet.alerts.safetynetalerts.rest.beans.Person;
 public class JsonDataLoader implements CommandLineRunner {
 	private static final Log logger = LogFactory.getLog(JsonDataLoader.class);
 
-   
-
     @Override
     public void run(String... args) throws Exception {
         loadJsonData();
@@ -33,38 +28,19 @@ public class JsonDataLoader implements CommandLineRunner {
 
     private void loadJsonData() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-//        ClassPathResource resource = new ClassPathResource("data.json");
-//        InputStream inputStream = resource.getInputStream();
         
+//      Load the JSON file
 		InputStream inputStream = getClass().getResourceAsStream("/data.json");
-            
-        
-//        Load the JSON file
-
+ 
 		JsonNode rootNode = objectMapper.readTree(inputStream);
 
-//		loadJsonData for person node();
-		JsonNode personsNode = rootNode.path("persons");
-		List<Person> personList = objectMapper.convertValue(personsNode, new TypeReference<List<Person>>() {});
-	
-		 
+		List<Person> personList = objectMapper.convertValue(rootNode.path("persons"), new TypeReference<List<Person>>() {});
 		logger.info("Person JSON data loaded.");
-
 	
 		List<FireStation> firestationList = objectMapper.convertValue(rootNode.path("firestations"), new TypeReference<List<FireStation>>() {});
-		
-
-	      
-	   		
-	    List<MedicalRecord> medicalrecordList = objectMapper.convertValue(rootNode.path("medicalrecords"), new TypeReference<List<MedicalRecord>>() {});
-
-             
-        
-//        List<Person> personsList = objectMapper.readValue(inputStream, new TypeReference<List<Person>>() {});
-
-//        repository.saveAll(entities);
-       
-        
-        logger.info("FireStation JSON data loaded.");
+		logger.info("FireStation JSON data loaded.");  	
+	   
+		List<MedicalRecord> medicalrecordList = objectMapper.convertValue(rootNode.path("medicalrecords"), new TypeReference<List<MedicalRecord>>() {});
+        logger.info("MedicalRecord JSON data loaded.");
     }
 }
